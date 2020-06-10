b03c95735987cda574d993bfda498db20ee76183
deepak.jha <deepak.jha@quovantis.com>
2020-06-10 20:48:06 +0530

    added html for allowance and call action page

---
+++ models/allowanceModel.js
@@ -0,0 +1,164 @@
+function AppViewModel() {
+  
+    var self = this;
+  
+    self.addRule = ko.observable(false);
+    self.showSearch = ko.observable(false);
+    self.showSelectOptions = ko.observable(false);
+    self.prioritypopover = ko.observable(false);
+    this.showSelectionTag = ko.observable(false);
+    self.show =  ko.observable(false);
+    self.showFilter = ko.observable(false);
+    self.radioSelectedOptionValue = ko.observable("singleNumber");
+    
+      self.showBox = function(){
+              self.show(true);
+       };
+       $("body").on("click", function(){
+           self.show(false);
+       });
+    
+    this.myAction= function() {
+       return true;
+    }  
+    self.showFilterList = function () {
+      self.showFilter(!self.showFilter());
+  };
+    self.showRule = function () {
+        self.addRule(true);
+    }
+    self.hideRule = function () {
+      self.addRule(false);
+  }
+    self.verifyPriority = function(){
+      self.prioritypopover(true);
+    }
+    self.setPriority = function(){
+      self.prioritypopover(false);
+    }
+    self.showSearchList = function () {
+        self.showSearch(true);
+    }
+    self.showDropList = function(){
+      self.showSelectOptions(true);
+  
+    }
+      this.counters = ko.observable([
+        {
+          'name': 'Specific A to Specific B',
+          'checked': false
+        },
+        {
+          'name': 'Specific A to Group B',
+          'checked': false
+        },
+        {
+          'name': 'Group A to Specif B',
+          'checked': false
+        },
+        {
+          'name':  'Group A to Group B',
+          'checked': false
+        }
+      ])
+      this.items = ko.observableArray([
+          {
+              id: 1,
+              allowanceName: 'A1-2233',
+              editedBy: 'John Darsey',
+              peroidicity: 'Bill Cycle',
+              originatingCalls : '500',
+              terminatingCalls : '1500',
+              totalCalls: '120',
+              totalCallsDuration: '2000',
+              originatingDuration: '2000',
+              terminatingDuration: '1500',
+              editedDate: '15-may-2020',
+          },
+          {
+            id: 2,
+            allowanceName: 'A1-2233',
+            editedBy: 'Vipin',
+            peroidicity: 'Bill Cycle',
+            originatingCalls : '500',
+            terminatingCalls : '1500',
+            totalCalls: '120',
+            totalCallsDuration: '2000',
+            originatingDuration: '2000',
+            terminatingDuration: '1500',
+            editedDate: '15-may-2020',
+          },
+          {
+            id: 3,
+            allowanceName: 'A1-2233',
+            editedBy: 'Modi',
+            peroidicity: 'Bill Cycle',
+            originatingCalls : '500',
+            terminatingCalls : '1500',
+            totalCalls: '120',
+            totalCallsDuration: '2000',
+            originatingDuration: '2000',
+            terminatingDuration: '1500',
+            editedDate: '15-may-2020',
+          }
+      ]);
+  
+          this.allowanceList = ko.observableArray([
+          {
+            id: 1,
+            templatesName: 'A1-1100',
+          },
+          {
+            id: 2,
+            templatesName: 'A2-1780',
+          },
+          {
+            id: 3,
+            templatesName: 'A3-1890',
+          },
+          {
+            id: 4,
+            templatesName: 'A4-1109',
+          },
+          {
+            id: 5,
+            templatesName: 'A5-1156',
+          },
+          {
+            id: 6,
+            templatesName: 'A6-1179',
+          }
+          ]);
+          this.thresholdList = ko.observableArray([
+          {
+            id: 11,
+            templatesName: 'b1-1100',
+          },
+          {
+            id: 22,
+            templatesName: 'b2-1780',
+          },
+          {
+            id: 33,
+            templatesName: 'b3-1890',
+          },
+          {
+            id: 44,
+            templatesName: 'b4-1109',
+          },
+          {
+            id: 55,
+            templatesName: 'B5-1156',
+          },
+          {
+            id: 60,
+            templatesName: 'B6-1179',
+          }
+        ]);
+        
+    }
+  
+  
+  // Activates knockout.js
+  ko.applyBindings(new AppViewModel());
+  
